9db849953365def1e735f1775eb0ef3b3ae4a6c4
Emil <emil.hagelin@medieinstitutet.se>
2021-11-16 13:46:07 +0100

    code changes

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const startGame = alert('Welcome to Cat Scratch, are you ready to find the cute cat?')
 
+function petHead() {
+    prompt('You won! GG EZ, the cat is yours, give it a name.')
+}
 
+function petBelly() {
+    alert('You lost! the cat hates bellyrubs and scratches you! Game Over.')
+}
 
 
 
